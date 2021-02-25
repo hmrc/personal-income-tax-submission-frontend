68f4f7130e15898aa87b2016b55cf2474474eade
AdamSmithHMRC <72032501+AdamSmithHMRC@users.noreply.github.com>
2021-02-25 11:07:09 +0000

    SASS-337-2 edited AgentAuthErrorPageViewSpec

--- test/views/helpers/AgentAuthErrorPageViewSpec.scala
+++ test/views/helpers/AgentAuthErrorPageViewSpec.scala
@@ -25,46 +25,28 @@ import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.i18n.{Messages, MessagesApi}
 import play.api.mvc.AnyContentAsEmpty
 import play.api.test.FakeRequest
-import views.html.authErrorPages.AgentAuthErrorPageView
 import utils.ViewTest
+import views.html.authErrorPages.AgentAuthErrorPageView
 
-class AgentAuthErrorPageViewSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite with ViewTest {
+class AgentAuthErrorPageViewSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite with ViewTest{
 
   object Selectors {
 
     val pageHeading = "#main-content > div > div > header > h1"
-    val p1 = "#main-content > div > div > p"
+    val p1 = "#main-content > div > div > p:nth-child(2)"
     val p2 = "#main-content > div > div > p:nth-child(3)"
     val link = "#client_auth_link"
   }
 
-  val agentAuthErrorPageView: AgentAuthErrorPageView = app.injector.instanceOf[AgentAuthErrorPageView]
-
-  implicit lazy val fakeRequest: FakeRequest[AnyContentAsEmpty.type] = FakeRequest("", "")
-  implicit lazy val messagesApi: MessagesApi = app.injector.instanceOf[MessagesApi]
-  implicit lazy val messages: Messages = messagesApi.preferred(fakeRequest)
-  implicit lazy val mockConfig: AppConfig = app.injector.instanceOf[AppConfig]
   lazy val pText1 = "You cannot view this client’s information." +
     " Your client needs to authorise you as their agent (open in a new tab) before you can sign into this service."
   lazy val pText2 = "Try another client’s details"
   lazy val linkText = "authorise you as their agent (open in a new tab)"
   lazy val href = "https://www.gov.uk/guidance/client-authorisation-an-overview"
 
-  def element(cssSelector: String)(implicit document: Document): Element = {
-    val elements = document.select(cssSelector)
-
-    if(elements.size == 0) {
-      fail(s"No element exists with the selector '$cssSelector'")
-    }
-
-    document.select(cssSelector).first()
-  }
-  def elementText(selector: String)(implicit document: Document): String = {
-    element(selector).text()
-  }
 
   "AgentAuthErrorPageView " should {
-    lazy val view = agentAuthErrorPageView()
+    lazy val view  = agentAuthErrorPageView()
     lazy implicit val document: Document = Jsoup.parse(view.body)
 
     "have a page heading of" in {
@@ -75,6 +57,7 @@ class AgentAuthErrorPageViewSpec extends AnyWordSpec with Matchers with GuiceOne
       elementText(Selectors.p1) shouldBe pText1
       elementText(Selectors.p2) shouldBe pText2
     }
+
     linkCheck(linkText, Selectors.link, href)
   }
 }
