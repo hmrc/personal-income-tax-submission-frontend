ad5a00b4ab04c52d8a0b3965926064e78477f876
AdamSmithHMRC <72032501+AdamSmithHMRC@users.noreply.github.com>
2021-04-22 14:19:39 +0100

    SASS-517 layout error

--- test/views/charity/GiftAidOverseasAmountViewSpec.scala
+++ test/views/charity/GiftAidOverseasAmountViewSpec.scala
@@ -53,7 +53,7 @@ class GiftAidOverseasAmountViewSpec extends ViewTest {
   val captionSelector = ".govuk-caption-l"
   val inputFieldSelector = "#amount"
   val buttonSelector = ".govuk-button"
-  val inputLabelSelector = ""
+  val inputLabelSelector = "#main-content > div > div > form > div > label > p"
   val inputHintTextSelector = ".govuk-hint"
 
   "GiftAidOverseasAmount with no errors" when {
