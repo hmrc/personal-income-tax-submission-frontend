570033f55c3e88e8ca7fd597a4002bafd302a7c0
AdamSmithHMRC <72032501+AdamSmithHMRC@users.noreply.github.com>
2021-05-04 10:56:40 +0100

    SASS-719 rebased and altered ISpec

--- it/controllers/charity/GiftAidSharesSecuritiesLandPropertyDonationControllerISpec.scala
+++ it/controllers/charity/GiftAidSharesSecuritiesLandPropertyDonationControllerISpec.scala
@@ -24,91 +24,95 @@ import org.jsoup.nodes.Document
 import play.api.http.HeaderNames
 import play.api.http.Status._
 import play.api.libs.ws.{WSClient, WSResponse}
-import utils.IntegrationTest
+import utils.{IntegrationTest,ViewHelpers}
 
-class GiftAidSharesSecuritiesLandPropertyDonationControllerISpec extends IntegrationTest {
+class GiftAidSharesSecuritiesLandPropertyDonationControllerISpec extends IntegrationTest with ViewHelpers {
 
   lazy val wsClient: WSClient = app.injector.instanceOf[WSClient]
   lazy val controller: GiftAidSharesSecuritiesLandPropertyDonationController = app.injector.instanceOf[GiftAidSharesSecuritiesLandPropertyDonationController]
 
-  object Selectors {
-
-    val heading = "h1"
-    val caption = ".govuk-caption-l"
-    val errorSummaryNoSelection = ".govuk-error-summary__body > ul > li > a"
-    val yesRadioButton = ".govuk-radios__item:nth-child(1) > label"
-    val noRadioButton = ".govuk-radios__item:nth-child(2) > label"
-
-  }
-
-  object Content {
-
-    val expectedTitleIndividual = "Did you donate shares, securities, land or property to charity? - Update and submit an Income Tax Return - GOV.UK"
-    val expectedTitleAgent = "Did your client donate shares, securities, land or property to charity? - Update and submit an Income Tax Return - GOV.UK"
-    val expectedErrorTitleIndividual =
-      "Error: Did you donate shares, securities, land or property to charity? - Update and submit an Income Tax Return - GOV.UK"
-    val expectedErrorTitleAgent =
-      "Error: Did your client donate shares, securities, land or property to charity? - Update and submit an Income Tax Return - GOV.UK"
-    val expectedH1Individual = "Did you donate shares, securities, land or property to charity?"
-    val expectedH1Agent = "Did your client donate shares, securities, land or property to charity?"
-    val expectedCaption = "Donations to charity for 6 April 2021 to 5 April 2022"
-    val noSelectionErrorIndividual = "Select yes if you donated shares, securities, land or property to charity"
-    val noSelectionErrorAgent = "Select yes if your client donated shares, securities, land or property to charity"
-    val yesText = "Yes"
-    val noText = "No"
-  }
-
   val taxYear: Int = 2022
+  val taxYearMinusOne: Int = taxYear - 1
+  val captionText = s"Donations to charity for 6 April $taxYearMinusOne to 5 April $taxYear"
+  val yesText = "Yes"
+  val noText = "No"
+  val continueText = "Continue"
+  val captionSelector = ".govuk-caption-l"
+  val continueSelector = "#continue"
+  val continueButtonFormSelector = "#main-content > div > div > form"
+  val errorSummaryHref = "#value"
+
+  object IndividualExpected {
+
+    val expectedTitle = "Did you donate shares, securities, land or property to charity?"
+    val expectedH1 = "Did you donate shares, securities, land or property to charity?"
+    val expectedError = "Select yes if you donated shares, securities, land or property to charity"
+    val expectedErrorTitle = s"Error: $expectedTitle"
+  }
+
+  object AgentExpected {
+    val expectedTitle = "Did your client donate shares, securities, land or property to charity?"
+    val expectedH1 = "Did your client donate shares, securities, land or property to charity?"
+    val expectedError = "Select yes if your client donated shares, securities, land or property to charity"
+    val expectedErrorTitle = s"Error: $expectedTitle"
+  }
 
   "as an individual" when {
+    import IndividualExpected._
 
-      ".show" should {
+    ".show is called" should {
 
-        "returns an action" which {
+      "returns a page" which {
+        lazy val result: WSResponse = {
+          authoriseIndividual()
+          await(wsClient.url(
+            s"http://localhost:$port/income-through-software/return/personal-income/$taxYear/charity/donation-of-shares-securities-land-or-property"
+          )
+            .get())
+        }
+
+        implicit def document: () => Document = () => Jsoup.parse(result.body)
+
+        "has an OK(200) status" in {
+          result.status shouldBe OK
+        }
+
+        "has the following page elements" which {
+          titleCheck(expectedTitle)
+          welshToggleCheck("English")
+          h1Check(expectedH1)
+          textOnPageCheck(captionText, captionSelector)
+          radioButtonCheck(yesText, 1)
+          radioButtonCheck(noText, 2)
+          buttonCheck(continueText, continueSelector)
+        }
+
+      }
+
+    }
+
+    ".submit is called" should {
+
+      s"return an OK($OK) status" when {
+
+        "there is form data" in {
+
           lazy val result: WSResponse = {
             authoriseIndividual()
             await(wsClient.url(
               s"http://localhost:$port/income-through-software/return/personal-income/$taxYear/charity/donation-of-shares-securities-land-or-property"
             )
-              .get())
-          }
-
-          lazy val document: Document = Jsoup.parse(result.body)
-
-          "has an OK(200) status" in {
-            result.status shouldBe OK
-          }
-
-          "displays the page content" in {
-
-            document.title() shouldBe Content.expectedTitleIndividual
-            document.select(Selectors.heading).text() shouldBe Content.expectedH1Individual
-            document.select(Selectors.caption).text() shouldBe Content.expectedCaption
-            document.select(Selectors.yesRadioButton).text() shouldBe Content.yesText
-            document.select(Selectors.noRadioButton).text() shouldBe Content.noText
-
-          }
-
-        }
-
-      }
-
-      ".submit" should {
-
-        s"return an OK($OK) status" in {
-          lazy val result: WSResponse = {
-            authoriseIndividual()
-            await(wsClient.url(
-                s"http://localhost:$port/income-through-software/return/personal-income/$taxYear/charity/donation-of-shares-securities-land-or-property"
-              )
-                .post(Map(YesNoForm.yesNo -> YesNoForm.yes))
+              .post(Map(YesNoForm.yesNo -> YesNoForm.yes))
             )
           }
 
           result.status shouldBe OK
         }
 
-        "when there is an incorrect input" should {
+        s"return an error page" when {
+
+          "there is no form data" which {
+
             lazy val result: WSResponse = {
               authoriseIndividual()
               await(wsClient.url(
@@ -117,18 +121,24 @@ class GiftAidSharesSecuritiesLandPropertyDonationControllerISpec extends Integra
                 .post(Map[String, String]()))
             }
 
-          lazy val document: Document = Jsoup.parse(result.body)
+            implicit def document: () => Document = () => Jsoup.parse(result.body)
 
-          s"return a BAD_REQUEST($BAD_REQUEST) status" in {
+            s"has a BAD_REQUEST($BAD_REQUEST) status" in {
+              result.status shouldBe BAD_REQUEST
+            }
 
-            result.status shouldBe BAD_REQUEST
+            s"has the following elements" which {
+              titleCheck(expectedErrorTitle)
+              welshToggleCheck("English")
+              h1Check(expectedH1)
+              textOnPageCheck(captionText, captionSelector)
+              errorSummaryCheck(expectedError, errorSummaryHref)
+              errorAboveElementCheck(expectedError)
+              radioButtonCheck(yesText, 1)
+              radioButtonCheck(noText, 2)
+              buttonCheck(continueText, continueSelector)
+            }
 
-          }
-
-          "have the correct page content" in {
-
-            document.title() shouldBe Content.expectedErrorTitleIndividual
-            document.select(Selectors.errorSummaryNoSelection).text() shouldBe Content.noSelectionErrorIndividual
           }
 
         }
@@ -137,11 +147,14 @@ class GiftAidSharesSecuritiesLandPropertyDonationControllerISpec extends Integra
 
     }
 
+  }
+
   "as an agent" when {
+    import AgentExpected._
 
-    ".show" should {
+    ".show is called" should {
 
-      "returns an action" which {
+      "returns a page" which {
         lazy val result: WSResponse = {
           lazy val sessionCookie: String = PlaySessionCookieBaker.bakeSessionCookie(Map[String, String](
             SessionValues.CLIENT_MTDITID -> "1234567890",
@@ -156,27 +169,27 @@ class GiftAidSharesSecuritiesLandPropertyDonationControllerISpec extends Integra
             .get())
         }
 
-        lazy val document: Document = Jsoup.parse(result.body)
+        implicit def document: () => Document = () => Jsoup.parse(result.body)
 
         "has an OK(200) status" in {
           result.status shouldBe OK
         }
 
-        "displays the page content" in {
-
-          document.title() shouldBe Content.expectedTitleAgent
-          document.select(Selectors.heading).text() shouldBe Content.expectedH1Agent
-          document.select(Selectors.caption).text() shouldBe Content.expectedCaption
-          document.select(Selectors.yesRadioButton).text() shouldBe Content.yesText
-          document.select(Selectors.noRadioButton).text() shouldBe Content.noText
-
+        "has the following page elements" which {
+          titleCheck(expectedTitle)
+          welshToggleCheck("English")
+          h1Check(expectedH1)
+          textOnPageCheck(captionText, captionSelector)
+          radioButtonCheck(yesText, 1)
+          radioButtonCheck(noText, 2)
+          buttonCheck(continueText, continueSelector)
         }
 
       }
 
     }
 
-    ".submit" should {
+    ".submit is called" should {
 
       s"return an OK($OK) status" when {
 
@@ -199,7 +212,7 @@ class GiftAidSharesSecuritiesLandPropertyDonationControllerISpec extends Integra
         }
       }
 
-      s"return a BAD_REQUEST($BAD_REQUEST) status" when {
+      s"return an error page" when {
 
         "there is no form data" in {
           lazy val result: WSResponse = {
@@ -216,7 +229,7 @@ class GiftAidSharesSecuritiesLandPropertyDonationControllerISpec extends Integra
               .post(Map[String, String]()))
           }
 
-          result.status shouldBe BAD_REQUEST
+            result.status shouldBe BAD_REQUEST
 
         }
 
@@ -240,12 +253,18 @@ class GiftAidSharesSecuritiesLandPropertyDonationControllerISpec extends Integra
             )
           }
 
-          lazy val document: Document = Jsoup.parse(result.body)
+          implicit def document: () => Document = () => Jsoup.parse(result.body)
 
-          "have the correct page content" in {
-
-            document.title() shouldBe Content.expectedErrorTitleAgent
-            document.select(Selectors.errorSummaryNoSelection).text() shouldBe Content.noSelectionErrorAgent
+          "has the following page elements" which {
+            titleCheck(expectedErrorTitle)
+            welshToggleCheck("English")
+            h1Check(expectedH1)
+            textOnPageCheck(captionText, captionSelector)
+            errorSummaryCheck(expectedError, errorSummaryHref)
+            errorAboveElementCheck(expectedError)
+            radioButtonCheck(yesText, 1)
+            radioButtonCheck(noText, 2)
+            buttonCheck(continueText, continueSelector)
           }
 
         }
