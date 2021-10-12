f2cf77b632ecfd9b3ff645b67b301b17810b3ca4
harrygull <15017045+HarryGull@users.noreply.github.com>
2021-10-12 10:43:16 +0100

    updated deoendencies again as updated available

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -20,7 +20,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.14.0",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.15.0",
     "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "1.19.0-play-28",
     "uk.gov.hmrc"             %% "govuk-template"             % "5.71.0-play-28",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.55.0",
@@ -28,7 +28,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.14.0"                 % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.15.0"                 % Test,
     "org.scalatest"           %% "scalatest"                % "3.2.9"                 % Test,
     "org.jsoup"               %  "jsoup"                    % "1.13.1"                % Test,
     "com.typesafe.play"       %% "play-test"                % current                 % Test,
