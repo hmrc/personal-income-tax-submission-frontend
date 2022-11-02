bb7909d6340f83e85970fcbfa36f96f08e2d0962
John Kelly <60345872+jhkldev@users.noreply.github.com>
2022-11-02 10:16:51 +0000

    SASS-3822: Update mongo test version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -36,6 +36,6 @@ object AppDependencies {
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                   % "test, it",
     "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.28.0"                  % "test, it",
     "org.scalamock"           %% "scalamock"                % "5.1.0"                   % Test,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.64.0"                  % "test, it"
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % hmrcMongoPlay28Version    % "test, it"
   )
 }
