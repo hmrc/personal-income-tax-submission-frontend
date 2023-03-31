b42adff19182665d55efd3a88e37e8e58a69eb3d
Kannan Ramakrishnan <12591116+kannanramakrishnan@users.noreply.github.com>
2023-03-31 18:45:39 +0100

    SASS-4001 Create How much did you get in stock dividends page

---
+++ app/controllers/dividends/StockDividendAmountController.scala
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.dividends
+
+import config.{AppConfig, DIVIDENDS, ErrorHandler}
+import controllers.predicates.CommonPredicates.commonPredicates
+import controllers.predicates.AuthorisedAction
+import forms.AmountForm
+import play.api.data.Form
+import play.api.i18n.I18nSupport
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import views.html.dividends.StockDividendAmountView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class StockDividendAmountController @Inject()(
+                                                  implicit val cc: MessagesControllerComponents,
+                                                  authAction: AuthorisedAction,
+                                                  view: StockDividendAmountView,
+                                                  errorHandler: ErrorHandler,
+                                                  implicit val appConfig: AppConfig,
+                                                  ec: ExecutionContext
+                                           ) extends FrontendController(cc) with I18nSupport {
+
+  def agentOrIndividual(implicit isAgent: Boolean): String = if (isAgent) "agent" else "individual"
+
+  def form(implicit isAgent: Boolean, taxYear: Int): Form[BigDecimal] = AmountForm.amountForm(
+    emptyFieldKey = "dividends.stock-dividend-amount.error.empty." + agentOrIndividual,
+    wrongFormatKey = "dividends.stock-dividend-amount.invalidFormat." + agentOrIndividual,
+    exceedsMaxAmountKey = "dividends.stock-dividend-amount.error.amountMaxLimit." + agentOrIndividual,
+    emptyFieldArguments = Seq(taxYear.toString)
+  )
+
+  def show(taxYear: Int): Action[AnyContent] = commonPredicates(taxYear, DIVIDENDS).async {implicit user =>
+      Future.successful(Ok(view(form(user.isAgent, taxYear), taxYear)))
+  }
+
+  def submit(taxYear: Int): Action[AnyContent] = commonPredicates(taxYear, DIVIDENDS).async { implicit user =>
+    form(user.isAgent, taxYear).bindFromRequest().fold(
+      formWithErrors =>
+        Future.successful(BadRequest(view(formWithErrors, taxYear))),
+      _ =>
+        Future.successful(Ok)
+    )
+  }
+
+}
