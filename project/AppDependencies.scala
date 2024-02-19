7a61a93eafea5b2f2a153706d1e903878b799dee
Rob Gresham <49005870+ghousedev@users.noreply.github.com>
2024-02-19 14:20:47 +0000

    SASS-7307: Update savings submission to match spec (#339)
    * SASS-7307: Update savings submission to match spec
    
    * SASS-7307: Update sbt-auto-build version
    
    * SASS-7307: Update play-frontend-hmrc

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -21,11 +21,11 @@ object AppDependencies {
 
   private val bootstrapPlay28Version = "8.2.0"
   private val hmrcMongoPlay28Version = "1.6.0"
-  private val hmrcPlayFrontend = "7.29.0-play-28"
+  private val hmrcPlayFrontend = "8.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-28" % bootstrapPlay28Version,
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc"         % hmrcPlayFrontend,
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-28" % hmrcPlayFrontend,
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-28"         % hmrcMongoPlay28Version,
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.14.2"
   )
