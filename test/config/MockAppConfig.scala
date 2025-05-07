e54990881cc59b628cf91875e7af39a3f205d4f7
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2025-05-07 18:17:52 +0100

    [SASS-11597] Add connector to income-tax-session-data µService

--- test/config/MockAppConfig.scala
+++ test/config/MockAppConfig.scala
@@ -30,6 +30,7 @@ class MockAppConfig extends AppConfig with MockFactory {
   override val interestBaseUrl: String = "/interest"
   override val giftAidBaseUrl: String = "/giftAid"
   override val incomeTaxSubmissionBEBaseUrl: String = "/incomeTaxSubmissionBaseUrl"
+  override val vcSessionServiceBaseUrl: String = "/incomeTaxSessionData"
 
   override def defaultTaxYear: Int = 2022
 
